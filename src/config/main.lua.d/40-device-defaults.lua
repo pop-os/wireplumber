bba68bd8d3cbc4e5877ca7d9e671892b7229cc00
13r0ck <bnr@tuta.io>
2022-05-03 20:56:12 -0600

    Reduce default volume

--- src/config/main.lua.d/40-device-defaults.lua
+++ src/config/main.lua.d/40-device-defaults.lua
@@ -5,6 +5,7 @@ device_defaults.properties = {
   -- when set to false, default nodes and routes are selected based on
   -- their priorities and any runtime changes do not persist after restart
   ["use-persistent-storage"] = true,
+  ["default-volume"] = 0.03,
 
   -- the default volume to apply to ACP device nodes, in the linear scale
   --["default-volume"] = 0.4,
